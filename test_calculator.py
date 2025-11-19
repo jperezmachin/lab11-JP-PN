704dd4d4d542fdf158e29904dbc94101e9b1b157
Prisha Narne <pmnarne@gmail.com>
2025-11-18 20:16:43 -0500

    merged changes

--- test_calculator.py
+++ test_calculator.py
@@ -52,5 +52,30 @@ class TestCalculator(unittest.TestCase):
     ##########################
 
 # Do not touch this
+import unittest
+import calculator
+
+    def test_multiply(self):
+        self.assertEqual(calculator.multiply(4,3),12)
+        self.assertEqual(calculator.multiply(-2, 3), -6)
+
+    def test_divide(self):
+        self.assertEqual(calculator.divide(10, 2),5)
+        self.assertEqual(calculator.divide(5,0), "Error: Division by zero")
+
+    def test_log_invalid_argument(self):
+        self.assertEqual(calculator.logarithm(0,0), "Logarithm inputs must be positive")
+        self.assertEqual(calculator.logarithm(2, 2),4)
+
+    def test_hypotenuse(self):
+        self.assertEqual(calculator.hypotenuse(2,2), math.sqrt(8))
+
+    def test_square_root(self):
+        self.assertEqual(calculator.square_root(-4), "Square root inputs must be positive")
+        self.assertEqual(calculator.square_root(4), 2)
+
+
+
+
 if __name__ == "__main__":
     unittest.main()
