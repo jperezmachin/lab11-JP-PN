13ef7dd29f7fd34f3553281df25e96dddd668ae5
Prisha Narne <pmnarne@gmail.com>
2025-11-18 20:52:17 -0500

    abcd

--- test_calculator.py
+++ test_calculator.py
@@ -3,6 +3,8 @@
 #Partner 2: Julio Perez
 import unittest
 from calculator import *
+import unittest
+import calculator
 
 class TestCalculator(unittest.TestCase):
 
@@ -11,37 +13,36 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(add(-1, 1), 0)
 
     def test_subtract(self):
-        self.assertEqual(sub(5, 3), 2)
-        self.assertEqual(sub(3, 5), -2)
+        self.assertEqual(subtract(5, 3), 2)
+        self.assertEqual(subtract(3, 5), -2)
 
     def test_divide_by_zero(self):
         with self.assertRaises(ZeroDivisionError):
             div(10, 0)
 
     def test_logarithm(self):
-        self.assertAlmostEqual(log(10, 100), 2.0)
-        self.assertAlmostEqual(log(2, 8), 3.0)
+        self.assertAlmostEqual(logarithm(10, 100), 2.0)
+        self.assertAlmostEqual(logarithm(2, 8), 3.0)
 
     def test_log_invalid_base(self):
         with self.assertRaises(ValueError):
-            log(1, 10)
+            logarithm(1, 10)
         with self.assertRaises(ValueError):
-            log(-2, 8)
+            logarithm(-2, 8)
         with self.assertRaises(ValueError):
-            log(10, -5)
+            logarithm(10, -5)
 
  
 # Do not touch this
-import unittest
-import calculator
+
 
     def test_multiply(self):
-        self.assertEqual(calculator.multiply(4,3),12)
-        self.assertEqual(calculator.multiply(-2, 3), -6)
+        self.assertEqual(calculator.mul(4,3),12)
+        self.assertEqual(calculator.mul(-2, 3), -6)
 
     def test_divide(self):
-        self.assertEqual(calculator.divide(10, 2),5)
-        self.assertEqual(calculator.divide(5,0), "Error: Division by zero")
+        self.assertEqual(calculator.div(10, 2),5)
+        self.assertEqual(calculator.div(5,0), "Error: Division by zero")
 
     def test_log_invalid_argument(self):
         self.assertEqual(calculator.logarithm(0,0), "Logarithm inputs must be positive")
