2142d93578625f95646a5fdb5e65aa267ef2b8ef
unknown <jucemachin06@gmail.com>
2025-11-18 19:43:40 -0500

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -1,11 +1,30 @@
+import math
 """
 calculator.py
 - Defines functions used to create a simple calculator
 
 One function per operation, in order.
 """
+
 # First example
 def add(a, b): 
-    pass
+    return a + b
 
+def sub(a, b): 
+    return a - b
 
+def mul(a, b): 
+    return a * b
+
+def div(a, b): # raise ZeroDivisionError if a == 0
+    if a == 0:
+        raise ZeroDivisionError
+
+    return b/a
+def log(a, b):# use math library + raise ValueError
+    if a <= 0 or b <= 0:
+        raise ValueError("Logarithm inputs must be positive.")
+    return math.log(b, a)  # log base a of b
+
+def exp(a, b):
+    return a ** b
